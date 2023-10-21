59b9997512da2d2543ab5bde26d6086844d5070c
mernishan <ishanrana500510@gmail.com>
2023-10-21 18:26:54 +0600

    x cordinet y cordinet done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import UseEffect from "./component/UseEffect";
+// import UseEffect from "./component/UseEffect";
+
+import UseEffectRun from "./component/UseEffectRun";
 
 const App = () => {
   return (
     <div>
-        <UseEffect/>
+        {/* <UseEffect/> */}
+        <UseEffectRun/>
     </div>
   );
 };
---
+++ src/component/UseEffectRun.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const UseEffectRun = () => {
+    const [x,setX] = useState(0);
+    const [y,setY] = useState(0);
+    const logMousePossition = e =>{
+        console.log("mouse event")
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+    useEffect(()=>{
+        console.log("UseEffect called")
+        window.addEventListener("mousemove",logMousePossition)
+    },[])
+        return (
+        <div>
+            Hooks x : {x} , y : {y}
+        </div>
+    );
+};
+
+export default UseEffectRun;
